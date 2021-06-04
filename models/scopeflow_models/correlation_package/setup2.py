0b744eadafd2483392e5d757e9303321ec55442e
cvlab cv6 <MY_NAME@example.com>
2021-06-04 21:22:17 +0900

    fit cuda_correlation setup install py in our env

--- models/scopeflow_models/correlation_package/setup2.py
+++ models/scopeflow_models/correlation_package/setup2.py
@@ -13,7 +13,7 @@ nvcc_args = [
     '-gencode', 'arch=compute_60,code=sm_60',
     '-gencode', 'arch=compute_61,code=sm_61',
     '-gencode', 'arch=compute_61,code=compute_61',
-    '-ccbin', '/usr/bin/gcc'
+    '-ccbin', '/usr/bin/gcc-5'
 ]
 
 setup(
@@ -22,7 +22,7 @@ setup(
         CUDAExtension('correlation_cuda', [
             'correlation_cuda.cc',
             'correlation_cuda_kernel.cu'
-        ], extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args, 'cuda-path': ['/usr/local/cuda']})
+        ], extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args, 'cuda-path': ['/usr/local/cuda-10.0']})
     ],
     cmdclass={
         'build_ext': BuildExtension
