052278fd74b951f5add7d6a6e896079fe7c31938
cvlab cv6 <MY_NAME@example.com>
2021-06-05 14:24:44 +0900

    configurate file fit for our data

---
+++ configs/config_EQVI_test_our_data.py
@@ -0,0 +1,19 @@
+# testset_root = '/data0/donghyeon/ipnn/AIM2020VTSRC/test'   # put your testing input folder here
+testset_root = '/data0/jwwoo/ipnn/AIM2020VTSRC/test/test_15fps'   # put your testing input folder here
+test_size = (1280, 720)          # speficy the frame resolution (640, 360) 
+test_crop_size = (1280, 720)
+
+mean = [0.0, 0.0, 0.0]
+std  = [1, 1, 1]
+
+inter_frames = 3     # number of interpolated frames
+preserve_input = True  # whether to preserve the input frames in the store path
+
+
+model = 'AcSloMoS_scope_unet_residual_synthesis_edge_LSE'  
+pwc_path = './utils/network-default.pytorch'
+
+
+store_path = 'outputs/running_test123'          # where to store the outputs
+checkpoint = 'checkpoints/Stage123_scratch/Stage123_scratch_checkpoint.ckpt'
+
